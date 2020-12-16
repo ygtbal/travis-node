3ebe756867a17b85f975f4871e2acb7a32b2d80f
yigitbalalan <yigitbalalan@gmail.com>
2020-12-16 23:40:46 +0300

    new

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server is running on PORT port`)
