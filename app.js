5460ab17aacef9192c8c70d20356bd5cd6189ea5
yigitbalalan <yigitbalalan@gmail.com>
2020-12-16 17:48:59 +0300

    hot fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.port || 4000;
 
 app.listen(port, () => {
-  console.log(`Server is running on PORT ${port}`)
+  console.log(`Server is running on PORT port`)
 });
 
 app.get('/', (req, res) => {
