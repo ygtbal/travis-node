7b8697e1c0985fd79814237c4b56e5579d25ed35
yigitbalalan <yigitbalalan@gmail.com>
2020-12-17 12:30:16 +0300

    eslint configuration added to travis ci

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
+
 const app = express();
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`Server is running on PORT port`)
+  console.log('Server is running on PORT port');
 });
 
-app.get('/', (req, res) => {
-  return res.json({
-    message: `Port is ${port}`
-  });
-});
+app.get('/', (req, res) => res.json({
+  message: `Port is ${port}`
+}));
 
 module.exports = app;
