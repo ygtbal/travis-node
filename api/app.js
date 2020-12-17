9e15b8c500df27180a1d34ef3272024f0d5f139f
yigitbalalan <yigitbalalan@gmail.com>
2020-12-17 17:42:45 +0300

    new commit for test system

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 import express from 'express';
+import db from './server/src/models';
 
 const app = express();
 
@@ -13,4 +14,13 @@ app.listen(port, () => {
 app.get('/', (req, res) => res.json({
   message: `Host is runnin on ${port}`
 }));
+
+app.get('/books', (req, res) => {
+  db.Book.findAll({}).then((data) => res.json({
+    data
+  })).catch((err) => res.json({
+    data: err
+  }));
+});
+
 module.exports = app;
--- api/test.js
+++ api/test.js
@@ -1,6 +1,8 @@
 /* eslint-disable linebreak-style */
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 
 const api = require('./app');
 
@@ -16,6 +18,16 @@ describe('main', () => {
       .end((err, res) => {
         res.should.have.status(200);
         res.should.be.a('object');
+        done();
+      });
+  });
+  // eslint-disable-next-line no-undef
+  it('get', (done) => {
+    chai.request(api)
+      .get('/')
+      .set('content-type', 'application/json')
+      .end((err, res) => {
+        res.should.have.status(200);
         done();
       });
   });
